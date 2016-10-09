c30ecec308aa5150847061835649d41235dab23f
Anan Dawsai <anan.daw@ascendcorp.com>
2016-10-09 16:39:23 +0700

    created project Driving Licence and created .gitignore

---
+++ DrivingLicence/app/src/main/java/licence/driving/app/android/slst/com/drivinglicence/LoadingActivity.java
@@ -0,0 +1,13 @@
+package licence.driving.app.android.slst.com.drivinglicence;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoadingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+    }
+}
